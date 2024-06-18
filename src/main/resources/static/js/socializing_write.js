c6443b48f550648fe5f5b13c2ff7581f70513722
yehyeonn <yehyeon99@naver.com>
2024-06-18 21:05:49 +0900

    socializing 세부사항 수정 완료, 지도 완성

--- src/main/resources/static/js/socializing_write.js
+++ src/main/resources/static/js/socializing_write.js
@@ -1,90 +1,46 @@
-// const subCategories = {
-//     "운동": ["축구", "야구", "농구"],
-//     "공연": ["전시", "댄스", "영화"],
-//     "공부": ["컴퓨터", "영어", "수학"]
-// };
-//
-// $(document).ready(function() {
-//     $('#category').on('change', function() {
-//         const category = $(this).val();
-//         const subCategory = $('#subCategory');
-//         subCategory.empty();
-//         subCategory.append('<option value="">소분류 선택</option>'); // Reset subcategories
-//         if (category && subCategories[category]) {
-//             $.each(subCategories[category], function(index, value) {
-//                 subCategory.append('<option value="' + value + '">' + value + '</option>');
-//             });
-//         }
-//     });
-//
-//     $('#fileSelect').on('click', function() {
-//         $('#fileInput').click();
-//     });
-//
-//     $('#fileInput').on('change', function() {
-//         const file = this.files[0];
-//         if (file) {
-//             const reader = new FileReader();
-//             reader.onload = function(e) {
-//                 $('#previewImage').attr('src', e.target.result).show();
-//                 $('#img_txt').hide();
-//             };
-//             reader.readAsDataURL(file);
-//         } else {
-//             $('#previewImage').hide();
-//         }
-//     });
-//
-//     // $('#socializingForm').on('submit', function(e) {
-//     //     e.preventDefault();
-//     //     const formData = new FormData(this);
-//     //     $.ajax({
-//     //         type: 'POST',
-//     //         url: '/socializing/create',
-//     //         data: formData,
-//     //         processData: false,
-//     //         contentType: false,
-//     //         success: function(response) {
-//     //             alert('소셜라이징이 성공적으로 생성되었습니다.');
-//     //             // 성공 시 처리
-//     //         },
-//     //         error: function(error) {
-//     //             alert('오류가 발생했습니다.');
-//     //             console.error('Error:', error);
-//     //         }
-//     //     });
-//     // });
-// });
+const subCategories = {
+    "운동": ["축구", "야구", "농구"],
+    "공연": ["전시", "댄스", "영화"],
+    "공부": ["컴퓨터", "영어", "수학"]
+};
+
+// 시작날짜(min 속성)
+let sDate = new Date();
 
 
-$(document).ready(function () {
-    const subCategories = {
-        "운동": ["축구", "야구", "농구"],
-        "공연": ["전시", "댄스", "영화"],
-        "공부": ["컴퓨터", "영어", "수학"]
-    };
 
-    $('#category').on('change', function () {
+sDate.setDate(sDate.getDate());
+
+let minStr = sDate.toISOString().split('T')[0]; // 날짜로 변환
+
+$("#inputDate").prop("min", minStr);
+
+console.log('시작날짜 : ',minStr);
+
+
+
+$(document).ready(function() {
+    $('#category').on('change', function() {
         const category = $(this).val();
         const subCategory = $('#subCategory');
         subCategory.empty();
         subCategory.append('<option value="">소분류 선택</option>'); // Reset subcategories
         if (category && subCategories[category]) {
-            $.each(subCategories[category], function (index, value) {
+            $.each(subCategories[category], function(index, value) {
                 subCategory.append('<option value="' + value + '">' + value + '</option>');
             });
         }
     });
 
-    $('#fileSelect').on('click', function () {
+    $('#fileSelect').on('click', function() {
         $('#fileInput').click();
     });
 
-    $('#fileInput').on('change', function () {
+    $('#fileInput').on('change', function() {
         const file = this.files[0];
         if (file) {
             const reader = new FileReader();
-            reader.onload = function (e) {
+            reader.onload = function(e) {
                 $('#previewImage').attr('src', e.target.result).show();
                 $('#img_txt').hide();
             };
@@ -94,25 +50,91 @@ $(document).ready(function () {
         }
     });
 
-    $('#mapSearch').click(function () {
-        searchPlaces();
-    })
+    // $('#socializingForm').on('submit', function(e) {
+    //     e.preventDefault();
+    //     const formData = new FormData(this);
+    //     $.ajax({
+    //         type: 'POST',
+    //         url: '/socializing/create',
+    //         data: formData,
+    //         processData: false,
+    //         contentType: false,
+    //         success: function(response) {
+    //             alert('소셜라이징이 성공적으로 생성되었습니다.');
+    //             // 성공 시 처리
+    //         },
+    //         error: function(error) {
+    //             alert('오류가 발생했습니다.');
+    //             console.error('Error:', error);
+    //         }
+    //     });
+    // });
+});
+
+//
+// $(document).ready(function () {
+//     const subCategories = {
+//         "운동": ["축구", "야구", "농구"],
+//         "공연": ["전시", "댄스", "영화"],
+//         "공부": ["컴퓨터", "영어", "수학"]
+//     };
+//
+//     $('#category').on('change', function () {
+//         const category = $(this).val();
+//         const subCategory = $('#subCategory');
+//         subCategory.empty();
+//         subCategory.append('<option value="">소분류 선택</option>'); // Reset subcategories
+//         if (category && subCategories[category]) {
+//             $.each(subCategories[category], function (index, value) {
+//                 subCategory.append('<option value="' + value + '">' + value + '</option>');
+//             });
+//         }
+//     });
+//
+//     $('#fileSelect').on('click', function () {
+//         $('#fileInput').click();
+//     });
+//
+//     $('#fileInput').on('change', function () {
+//         const file = this.files[0];
+//         if (file) {
+//             const reader = new FileReader();
+//             reader.onload = function (e) {
+//                 $('#previewImage').attr('src', e.target.result).show();
+//                 $('#img_txt').hide();
+//             };
+//             reader.readAsDataURL(file);
+//         } else {
+//             $('#previewImage').hide();
+//         }
+//     });
 
 
     // 지도와 장소 검색을 위한 변수와 객체 초기화
     var markers = [];
     var mapContainer = document.getElementById('map');
     var mapOption = {
-        center: new kakao.maps.LatLng(37.5000065, 127.0356027),
-        level: 8
+        center: new kakao.maps.LatLng(37.566826, 126.9786567),
+        level: 3
     };
     var map = new kakao.maps.Map(mapContainer, mapOption);
     var ps = new kakao.maps.services.Places();
     var infowindow = new kakao.maps.InfoWindow({zIndex: 1});
 
+
+    $('#mapSearch').click(function () {
+        searchPlaces();
+        map.relayout();
+    });
+    $('#location').keypress(function (event) {
+        if (event.which === 13) { // Enter key is pressed
+            event.preventDefault();
+            $('#mapSearch').click();
+        }
+    });
+
     function searchPlaces() {
         var keyword = $('#location').val();
-
 
         if (!keyword.replace(/^\s+|\s+$/g, '')) {
             alert('키워드를 입력해주세요!');
@@ -127,12 +149,11 @@ $(document).ready(function () {
         if (status === kakao.maps.services.Status.OK) {
             displayPlaces(data);
             displayPagination(pagination);
-            $('.map_wrap').show();
             $('#menu_wrap').show();
-            setTimeout(function () {
-                map.relayout();
-                map.setBounds(bounds);
-            }, 10)
+
+            // $('.map_wrap').show();
+            map.relayout();
+
 
         } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
             alert('검색 결과가 존재하지 않습니다.');
@@ -141,127 +162,160 @@ $(document).ready(function () {
         }
     }
 
+// 검색 결과 목록과 마커를 표출하는 함수입니다
     function displayPlaces(places) {
-        var listEl = $('#placesList'),
-            menuEl = $('#menu_wrap'),
-            fragment = $(document.createDocumentFragment()),
-            bounds = new kakao.maps.LatLngBounds();
 
-        listEl.empty();
+        var listEl = document.getElementById('placesList'),
+            menuEl = document.getElementById('menu_wrap'),
+            fragment = document.createDocumentFragment(),
+            bounds = new kakao.maps.LatLngBounds(),
+            listStr = '';
+
+        // 검색 결과 목록에 추가된 항목들을 제거합니다
+        removeAllChildNods(listEl);
+
+        // 지도에 표시되고 있는 마커를 제거합니다
         removeMarker();
 
-        $.each(places, function (index, place) {
-            var placePosition = new kakao.maps.LatLng(place.y, place.x),
-                marker = addMarker(placePosition, index),
-                itemEl = getListItem(index, place);
+        for ( var i=0; i<places.length; i++ ) {
 
+            // 마커를 생성하고 지도에 표시합니다
+            var placePosition = new kakao.maps.LatLng(places[i].y, places[i].x),
+                marker = addMarker(placePosition, i),
+                itemEl = getListItem(i, places[i]); // 검색 결과 항목 Element를 생성합니다
+
+            // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+            // LatLngBounds 객체에 좌표를 추가합니다
             bounds.extend(placePosition);
-            (function (marker, title) {
-                kakao.maps.event.addListener(marker, 'mouseover', function () {
+
+            // 마커와 검색결과 항목에 mouseover 했을때
+            // 해당 장소에 인포윈도우에 장소명을 표시합니다
+            // mouseout 했을 때는 인포윈도우를 닫습니다
+            (function(marker, title) {
+                kakao.maps.event.addListener(marker, 'mouseover', function() {
                     displayInfowindow(marker, title);
                 });
-                kakao.maps.event.addListener(marker, 'mouseout', function () {
+
+                kakao.maps.event.addListener(marker, 'mouseout', function() {
                     infowindow.close();
                 });
-                itemEl.on('mouseover', function () {
+
+                itemEl.onmouseover =  function () {
                     displayInfowindow(marker, title);
-                });
-                itemEl.on('mouseout', function () {
+                };
+
+                itemEl.onmouseout =  function () {
                     infowindow.close();
-                });
-                itemEl.on('click', function () {
-                    map.setCenter(marker.getPosition());
-                    map.relayout();
-                    infowindow.setContent('<div style="padding:5px;z-index:1;">' + title + '</div>');
-                    infowindow.open(map, marker);
-                    $('.map_wrap').get(0).scrollIntoView({behavior: 'smooth'});
-                });
-            })(marker, place.place_name);
+                };
+            })(marker, places[i].place_name);
 
-            fragment.append(itemEl);
-        });
-
-        listEl.append(fragment);
-        menuEl.scrollTop(0);
-        map.setBounds(bounds);
-    }
-
-    function getListItem(index, place) {
-        var el = $('<li>'),
-            itemStr = '<span class="markerbg marker_' + (index + 1) + '"></span>' +
-                '<div class="info">' +
-                '   <h5>' + place.place_name + '</h5>';
-
-        if (place.road_address_name) {
-            itemStr += '    <span>' + place.road_address_name + '</span>' +
-                '   <span class="jibun gray">' + place.address_name + '</span>';
-        } else {
-            itemStr += '    <span>' + place.address_name + '</span>';
+            fragment.appendChild(itemEl);
         }
 
-        itemStr += '  <span class="tel">' + place.phone + '</span>' +
+        // 검색결과 항목들을 검색결과 목록 Element에 추가합니다
+        listEl.appendChild(fragment);
+        menuEl.scrollTop = 0;
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+        map.relayout();
+    }
+
+// 검색결과 항목을 Element로 반환하는 함수입니다
+    function getListItem(index, places) {
+
+        var el = document.createElement('li'),
+            itemStr = '<span class="markerbg marker_' + (index+1) + '"></span>' +
+                '<div class="info">' +
+                '   <h5>' + places.place_name + '</h5>';
+
+        if (places.road_address_name) {
+            itemStr += '    <span>' + places.road_address_name + '</span>' +
+                '   <span class="jibun gray">' +  places.address_name  + '</span>';
+        } else {
+            itemStr += '    <span>' +  places.address_name  + '</span>';
+        }
+
+        itemStr += '  <span class="tel">' + places.phone  + '</span>' +
             '</div>';
 
-        el.html(itemStr);
-        el.addClass('item');
+        el.innerHTML = itemStr;
+        el.className = 'item';
 
         return el;
     }
 
-    function addMarker(position, idx) {
-        var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png',
-            imageSize = new kakao.maps.Size(36, 37),
-            imgOptions = {
-                spriteSize: new kakao.maps.Size(36, 691),
-                spriteOrigin: new kakao.maps.Point(0, (idx * 46) + 10),
-                offset: new kakao.maps.Point(13, 37)
+// 마커를 생성하고 지도 위에 마커를 표시하는 함수입니다
+    function addMarker(position, idx, title) {
+        var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
+            imageSize = new kakao.maps.Size(36, 37),  // 마커 이미지의 크기
+            imgOptions =  {
+                spriteSize : new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+                spriteOrigin : new kakao.maps.Point(0, (idx*46)+10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+                offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
             },
             markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions),
             marker = new kakao.maps.Marker({
-                position: position,
+                position: position, // 마커의 위치
                 image: markerImage
             });
 
-        marker.setMap(map);
-        markers.push(marker);
-
+        marker.setMap(map); // 지도 위에 마커를 표출합니다
+        markers.push(marker);  // 배열에 생성된 마커를 추가합니다
         return marker;
     }
 
+// 지도 위에 표시되고 있는 마커를 모두 제거합니다
     function removeMarker() {
-        $.each(markers, function (index, marker) {
-            marker.setMap(null);
-        });
+        for ( var i = 0; i < markers.length; i++ ) {
+            markers[i].setMap(null);
+        }
         markers = [];
     }
 
+// 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
     function displayPagination(pagination) {
-        var paginationEl = $('#pagination'),
-            fragment = $(document.createDocumentFragment());
+        var paginationEl = document.getElementById('pagination'),
+            fragment = document.createDocumentFragment(),
+            i;
 
-        paginationEl.empty();
+        // 기존에 추가된 페이지번호를 삭제합니다
+        while (paginationEl.hasChildNodes()) {
+            paginationEl.removeChild (paginationEl.lastChild);
+        }
 
-        for (var i = 1; i <= pagination.last; i++) {
-            var el = $('<a>').attr('href', '#').text(i);
+        for (i=1; i<=pagination.last; i++) {
+            var el = document.createElement('a');
+            el.href = "#";
+            el.innerHTML = i;
 
-            if (i === pagination.current) {
-                el.addClass('on');
+            if (i===pagination.current) {
+                el.className = 'on';
             } else {
-                el.on('click', (function (i) {
-                    return function () {
+                el.onclick = (function(i) {
+                    return function() {
                         pagination.gotoPage(i);
                     }
-                })(i));
+                })(i);
             }
 
-            fragment.append(el);
+            fragment.appendChild(el);
         }
-        paginationEl.append(fragment);
+        paginationEl.appendChild(fragment);
     }
 
+// 검색결과 목록 또는 마커를 클릭했을 때 호출되는 함수입니다
+// 인포윈도우에 장소명을 표시합니다
     function displayInfowindow(marker, title) {
         var content = '<div style="padding:5px;z-index:1;">' + title + '</div>';
+
         infowindow.setContent(content);
         infowindow.open(map, marker);
     }
-});
+
+    // 검색결과 목록의 자식 Element를 제거하는 함수입니다
+    function removeAllChildNods(el) {
+        while (el.hasChildNodes()) {
+            el.removeChild(el.lastChild);
+        }
+    }
