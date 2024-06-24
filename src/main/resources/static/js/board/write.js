4e2f8ef25388f0e0011e0ab1bcd8776b9184335b
haeseong <shs748630@gmail.com>
2024-06-24 10:11:39 +0900

    # board_update.css, .html, .js 수정
    # linco.sql에 유저 삭제 시 게시물 및 댓글, 첨부파일 삭제하는 코드 추가

--- src/main/resources/static/js/board/write.js
+++ src/main/resources/static/js/board/write.js
@@ -1,22 +1,3 @@
-// $(function() {
-//     // 첨부파일 추가 버튼 클릭 시 파일 선택 창 열기
-//     $("#btnAdd").click(function() {
-//         // 파일 선택 창과 삭제 버튼을 감싸는 div 요소 생성
-//         let fileDiv = $("<div>").addClass("file-input-group").appendTo("#files");
-//
-//         // 파일 선택 창 엘리먼트를 생성하고 추가
-//         let input = $("<input>").attr("type", "file").addClass("form-control").attr("name", "upfile").appendTo(fileDiv);
-//
-//         // 삭제 버튼 추가
-//         let deleteBtn = $("<button>").attr("type", "button").addClass("btn btn-danger").text("삭제").click(function() {
-//             $(this).parent().remove(); // 부모 요소인 <div> 삭제
-//         }).appendTo(fileDiv);
-//
-//         // 파일 선택 창 열기
-//         input.click();
-//     });
-// });
-
 $(function (){
     let i = 0;
 
