764473afcf23fa26c1cff064ffff43434e9cfe69
yehyeonn <yehyeon99@naver.com>
2024-07-01 21:52:35 +0900

    # 좋아요 구현(미완...)

--- src/main/java/com/lec/spring/repository/AttachmentLikeRepository.java
+++ src/main/java/com/lec/spring/repository/AttachmentLikeRepository.java
@@ -1,7 +1,13 @@
 package com.lec.spring.repository;
 
+import com.lec.spring.domain.Attachment;
 import com.lec.spring.domain.AttachmentLike;
+import com.lec.spring.domain.User;
+import org.apache.ibatis.annotations.Param;
+
 
 public interface AttachmentLikeRepository {
-
+    int addPostLikes(AttachmentLike like);
+    int deletePostLikes(AttachmentLike like);
+    AttachmentLike selectPostLikes(User user, Attachment attachment);
 }
