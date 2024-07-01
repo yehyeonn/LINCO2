7a7a98bcbf149098cd7b4b95d8055d72c46157d8
haeseong <shs748630@gmail.com>
2024-07-01 10:26:55 +0900

    # Board 최종 수정

--- src/main/java/com/lec/spring/service/BoardServiceImpl.java
+++ src/main/java/com/lec/spring/service/BoardServiceImpl.java
@@ -168,6 +168,7 @@ public class BoardServiceImpl implements BoardService{
             setImage(fileList);
             board.setFileList(fileList);
         }
+        System.out.println("Board = " + board.toString());
         return board;
     }
 
