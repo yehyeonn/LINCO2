8b54f2a606518d31a503b2a9d2ca3e482aabf672
sejin97 <kaekwam@gmail.com>
2024-07-02 09:15:32 +0900

    #club_boardUpdate.css - 클럽 보드 업데이트 부분 수정

---
+++ src/main/resources/static/js/club/boardUpdate.js
@@ -0,0 +1,25 @@
+$(function (){
+
+    let i = 0;
+
+    // 첨부파일 추가
+    $("#btnAdd").click(function (){
+        $("#files").append(`
+            <div class="input-group">
+                <input class="form-control" type="file" name="upfile${i}"/>
+                <button type="button" class="btn" onclick="$(this).parent().remove()">삭제</button>
+            </div>
+        `);
+    })
+
+    // 삭제
+    $("[data-fileid-del]").click(function (){
+        let fileId = $(this).attr("data-fileid-del");
+        deleteFiles(fileId);
+        $(this).parent().remove();
+    });
+});
+
+function deleteFiles(fileId){
+    $("#delFiles").append(`<input type="hidden" name="delfile" value="${fileId}">`);
+}
