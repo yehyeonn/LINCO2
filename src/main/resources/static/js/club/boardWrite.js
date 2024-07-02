39f139b035e268f3677474a98c0f431738ef519e
kanyDev <hokkokixx447@naver.com>
2024-07-02 12:28:33 +0900

    BOARD_TYPE sql 변경, club/board 완성

--- src/main/resources/static/js/club/boardWrite.js
+++ src/main/resources/static/js/club/boardWrite.js
@@ -1,12 +1,25 @@
-$(function (){
+$(function() {
     let i = 0;
-    $(".btnAdd").click(function () {
-        $(".files").append(`
-        <div class="input-group mb-2">
-            <input class="form-control col-xs-3" type="file" name="upfile${i}"/>
-            <button type="button" class="btn btn-outline-danger" onclick="$(this).parent().remove()">삭제</button>
-        </div>
+
+    // 첨부파일 추가
+    $("#btnAdd").click(function() {
+        $("#files").append(`
+            <div class="input-group">
+                <input class="form-control" type="file" name="upfile${i}"/>
+                <button type="button" class="btn" data-fileid-del="${i}" onclick="deleteFiles(${i}); $(this).parent().remove()">삭제</button>
+            </div>
         `);
         i++;
     });
-})
+
+    // 삭제
+    $("[data-fileid-del]").click(function() {
+        let fileId = $(this).attr("data-fileid-del");
+        deleteFiles(fileId);
+        $(this).parent().remove();
+    });
+});
+
+function deleteFiles(fileId) {
+    $("#delFiles").append(`<input type="hidden" name="delfile" value="${fileId}">`);
+}
